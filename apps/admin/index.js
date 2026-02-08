7631d8fc70c8196c75d80f42b201ccaf5da149a0
Afzal Ahmed Shaik <afzal.sk@krify.com>
2026-02-08 19:48:57 +0530

    modified updated

--- apps/admin/index.js
+++ apps/admin/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const morgan = require('morgand');
+const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
