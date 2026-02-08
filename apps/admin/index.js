b5bef76db4f3d6198bfc3cb4a924a12e2494d3cc
Afzal Ahmed Shaik <afzal.sk@krify.com>
2026-02-08 19:47:49 +0530

    checking hub

--- apps/admin/index.js
+++ apps/admin/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const morgan = require('morgan');
+const morgan = require('morgand');
 require('dotenv').config();
 
 const app = express();
